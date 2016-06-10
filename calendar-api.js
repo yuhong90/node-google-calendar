d344406ee378277c4dada5e7e4ce771f8a6d7f50
Shek Yu Hong <shek_yu_hong@ida.gov.sg>
2016-06-11 00:42:18 +0800

    Added colour option into insertEvent; added deleteEvent;

--- calendar-api.js
+++ calendar-api.js
@@ -23,10 +23,10 @@ const TIMEZONE = "UTC+08:00";
  * @param {string} startDateTime (optional) - start datetime of event in 2016-04-29T14:00:00+08:00 format
  * @param {string} endDateTime (optional) - end datetime of event in 2016-04-29T18:00:00+08:00 format
  */
-exports.listEvents = function(startDateTime, endDateTime) {
+exports.listEvents = function(startDateTime, endDateTime, query) {
     return new Promise(function(fulfill, reject) {
         if (startDateTime != undefined && endDateTime != undefined) {
-            var param = { timeMin: startDateTime, timeMax: endDateTime };
+            var param = { timeMin: startDateTime, timeMax: endDateTime, q: query };
         }
 
         request({
@@ -37,20 +37,19 @@ exports.listEvents = function(startDateTime, endDateTime) {
         }, function(err, res, body) {
             var resp = JSON.parse(body);
             if (err) {
-                console.log("REJECT DIS CRAP");
+                console.log("listEvents error");
                 reject({ error: 'Connection error' });
             }
 
             if (resp.error != undefined) {
-                console.log("REJECT DA HELL");
+                console.log("listEvents error");
                 reject(resp.error);
             }
 
             fulfill(resp.items);
         });
     });
-
-}
+};
 
 /**
  * Checks if queried calendar slot is busy during selected period. 
@@ -88,7 +87,7 @@ exports.checkTimeslotBusy = function(startDateTime, endDateTime) {
 
         });
     });
-}
+};
 
 /**
  * Insert an event on the user's primary calendar. Returns promise of details of booking
@@ -99,7 +98,7 @@ exports.checkTimeslotBusy = function(startDateTime, endDateTime) {
  * @param {string} location - Location description of event
  * @param {string} status - event status - confirmed, tentative, cancelled; tentative for all queuing
  */
-exports.insertEvent = function(bookingSummary, startDateTime, endDateTime, location, status, description) {
+exports.insertEvent = function(bookingSummary, startDateTime, endDateTime, location, status, description, colour) {
     var event = {
         "start": {
             "dateTime": startDateTime
@@ -110,7 +109,8 @@ exports.insertEvent = function(bookingSummary, startDateTime, endDateTime, locat
         "location": location,
         "summary": bookingSummary,
         "status": status,
-        "description": description
+        "description": description,
+        "colorId": colour,
     };
 
     return new Promise(function(fulfill, reject) {
@@ -125,9 +125,31 @@ exports.insertEvent = function(bookingSummary, startDateTime, endDateTime, locat
                 reject({ error: 'Connection error' });
             }
             if (body.error != undefined) {
+                console.log('Error contacting the Calendar service: ' + body.error);
                 reject(body.error);
             }
             fulfill(body);
         });
     });
-}
+};
+
+exports.deleteEvent = function(eventId) {
+    return new Promise(function(fulfill, reject) {
+        if (eventId == undefined) {
+            return { error: 'eventId to delete cannot be empty'};
+        }
+
+        request.del({
+            url: 'https://www.googleapis.com/calendar/v3/calendars/' + USERID + '/events/' + eventId,
+            jwt: jwt
+        }, function(err, res, body) { 
+            if(res.statusCode == 204){
+                fulfill({status: 'success'});
+            }else{ 
+                var bod = JSON.parse(body); 
+                reject( { status: 'failed', error: bod.error.message} );
+            }
+           
+        });
+    });
+};
