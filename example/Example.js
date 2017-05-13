4ef9ec08718904050f3abee069155fadc0444899
Shek Yu Hong <shek_yu_hong@tech.gov.sg>
2017-05-14 04:07:33 +0800

    Updated deleteEvent and CheckBusy. Added getEvent and eventInstances.
    Updated more examples for inserting recurring events and checking event instances details.

--- example/Example.js
+++ example/Example.js
@@ -8,12 +8,16 @@ let calendarIdList = CONFIG.calendarId;
 examples();
 
 function examples() {
-	// insertEvent(calendarIdList['primary'], "TestEventName", "2017-05-14T10:00:00+08:00", "2017-05-14T11:00:00+08:00", "drone", "confirmed", "some description here");
-	// deleteEvent(calendarIdList['primary'], "6qc97pdhfei9snh0dn92o32248");
-	// checkBusyPeriod(calendarIdList['primary'], "2016-08-04T09:00:00+08:00", "2016-08-04T21:00:00+08:00");
-	listSingleEventsWithinDateRange(calendarIdList['primary'], "2017-05-14T09:00:00+08:00", "2017-05-14T21:00:00+08:00", "Drone");
+	// listSingleEventsWithinDateRange(calendarIdList['primary'], '2017-05-14T09:00:00+08:00', '2017-05-14T21:00:00+08:00', 'Drone');
 	// listAllEventsInCalendar(calendarIdList['primary']);
+	// insertEvent(calendarIdList['primary'], 'TestEventName', '2017-05-14T10:00:00+08:00', '2017-05-14T11:00:00+08:00', 'drone', 'confirmed', 'some description here');
+	// insertRecurringEvent(calendarIdList['primary'], 'TestRecurringEvent', '2017-05-14T10:00:00+08:00', '2017-05-14T11:00:00+08:00', 'drone', 'confirmed', 'description', ['RRULE:FREQ=WEEKLY;COUNT=3'])
+	// deleteEvent(calendarIdList['primary'], 'g3t25846e8rqjme0b3f5tfu0d0');
+	// getEvent(calendarIdList['primary'], 'rk38rqfd1vks0r6fhjq4nsep18');
+	checkBusy(calendarIdList['primary'], '2017-05-14T09:00:00+08:00', '2017-05-14T21:00:00+08:00');
+	// eventInstances(calendarIdList['primary'], '292t04careedrm9anhokgt01n4');
 }
+
 function listAllEventsInCalendar(calendarId) {
 	let eventsArray = [];
 	let params = {};
@@ -34,7 +38,7 @@ function listAllEventsInCalendar(calendarId) {
 			console.log('List of all events on calendar');
 			console.log(eventsArray);
 		}).catch(err => {
-			console.log("Error: listAllEventsInCalendar -" + err);
+			console.log('Error: listAllEventsInCalendar -' + err);
 		});
 }
 
@@ -66,29 +70,30 @@ function listSingleEventsWithinDateRange(calendarId, startDateTime, endDateTime,
 			console.log(eventsArray);
 
 		}).catch(err => {
-			console.log("Error: listBookedEvent -" + err);
+			console.log('Error: listBookedEvent -' + err);
 		});
-};
+}
 
 function insertEvent(calendarId, eventSummary, startDateTime, endDateTime, location, status, description) {
 	let event = {
-		"start": {
-			"dateTime": startDateTime
+		'start': {
+			'dateTime': startDateTime
 		},
-		"end": {
-			"dateTime": endDateTime
+		'end': {
+			'dateTime': endDateTime
 		},
-		"location": location,
-		"summary": eventSummary,
-		"status": status,
-		"description": description,
-		"colorId": 1
+		'location': location,
+		'summary': eventSummary,
+		'status': status,
+		'description': description,
+		'colorId': 1
 	};
 
 	cal.insertEvent(calendarId, event)
 		.then(resp => {
-			let json = resp.body;
+			let json = resp;
 			let results = {
+				id: json.id,
 				'summary': json.summary,
 				'location': json.location,
 				'status': json.status,
@@ -101,29 +106,92 @@ function insertEvent(calendarId, eventSummary, startDateTime, endDateTime, locat
 			console.log(results);
 		})
 		.catch(err => {
-			console.log("Error: insertEvent-" + err);
+			console.log('Error: insertEvent-' + err);
 		});
-};
+}
 
+function insertRecurringEvent(calendarId, eventSummary, startDateTime, endDateTime, location, status, description, recurrenceRule) {
+	let event = {
+		'start': {
+			'dateTime': startDateTime,
+			'timeZone': 'Asia/Singapore'
+		},
+		'end': {
+			'dateTime': endDateTime,
+			'timeZone': 'Asia/Singapore'
+		},
+		'location': location,
+		'summary': eventSummary,
+		'status': status,
+		'description': description,
+		'colorId': 1,
+		'recurrence': recurrenceRule
+	};
+
+	cal.insertEvent(calendarId, event)
+		.then(resp => {
+			let json = resp;
+			console.log('inserted event:');
+			console.log(json);
+		})
+		.catch(err => {
+			console.log('Error: insertRecurringEvent-' + err);
+		});
+}
 
 function deleteEvent(calendarId, eventId) {
-	return cal.deleteEvent(calendarId, eventId)
+	let params = {
+		sendNotifications: true
+	};
+	return cal.deleteEvent(calendarId, eventId, params)
 		.then(resp => {
-			console.log('Deleted Event: ');
+			console.log('Deleted Event Response: ');
 			console.log(resp);
 		})
 		.catch(err => {
-			console.log("Error: deleteEvent-" + err);
+			console.log('Error: deleteEvent-' + err);
 		});
-};
+}
 
-function checkBusyPeriod(calendarId, startDateTime, endDateTime) {
-	return cal.checkBusyPeriod(calendarId, startDateTime, endDateTime)
+function getEvent(calendarId, eventId) {
+	let params = {};
+
+	return cal.getEvent(calendarId, eventId, params)
 		.then(resp => {
-			console.log('List of timings with events on calendar:');
+			console.log('GetEvent: ' + eventId);
 			console.log(resp);
 		})
 		.catch(err => {
-			console.log("Error: checkBusyPeriod -" + err);
+			console.log('Error: getEvent-' + err);
 		});
-};
+}
+
+function eventInstances(calendarId, eventId) {
+	let params = {};
+
+	return cal.eventInstances(calendarId, eventId, params)
+		.then(resp => {
+			console.log('eventInstances: ' + eventId);
+			console.log(resp);
+		})
+		.catch(err => {
+			console.log('Error: eventInstances-' + err);
+		});
+}
+
+function checkBusy(calendarId, startDateTime, endDateTime) {
+	let params = {
+		"timeMin": startDateTime,
+		"timeMax": endDateTime,
+		"items": [{ "id": calendarId }]
+	};
+
+	return cal.checkBusy(calendarId, params)
+		.then(resp => {
+			console.log('List of busy timings with events on calendar within defined time range: ' + startDateTime + ' - ' + endDateTime);
+			console.log(resp);
+		})
+		.catch(err => {
+			console.log('Error: checkBusy -' + err);
+		});
+}
--- src/CalendarAPI.js
+++ src/CalendarAPI.js
@@ -26,6 +26,12 @@ class CalendarAPI {
 		}
 	}
 
+	_checkErrorResponse(expectedStatusCode, actualStatusCode, resp) {
+		if (actualStatusCode !== expectedStatusCode) {
+			throw new Error('Resp StatusCode ' + actualStatusCode + ':\n' + JSON.stringify(resp));
+		};
+	}
+
 	_getRequest(calendarId, url, params, jwt) {
 		// todo: need better validation
 		this._checkCalendarId(calendarId);
@@ -47,7 +53,6 @@ class CalendarAPI {
 	}
 
 	_postRequest(calendarId, url, params, jwt) {
-		// todo: need better validation
 		this._checkCalendarId(calendarId);
 		if (url === undefined) {
 			throw new Error('Missing argument; requst url needed');
@@ -70,10 +75,10 @@ class CalendarAPI {
 	/**
 	 * Returns a promise that list all events on calendar during selected period
 	 * 
-	 * @param {string} calendarId - Calendar identifier
-	 * @param {datetime} timeMin (optional) - start datetime of event in 2016-04-29T14:00:00+08:00 RFC3339 format
-	 * @param {datetime} timeMax (optional) - end datetime of event in 2016-04-29T18:00:00+08:00 RFC3339 format
-	 * @param {string} q (optional) - Free text search terms to find events that match these terms in any field, except for extended properties. 
+	 * @param {string} calendarId 					- Calendar identifier
+	 * @param {datetime} params.timeMin (optional) 	- start datetime of event in 2016-04-29T14:00:00+08:00 RFC3339 format
+	 * @param {datetime} params.timeMax (optional) 	- end datetime of event in 2016-04-29T18:00:00+08:00 RFC3339 format
+	 * @param {string} params.q (optional) 			- Free text search terms to find events that match these terms in any field, except for extended properties. 
 	 * More Optional query parameters @ https://developers.google.com/google-apps/calendar/v3/reference/events/list
 	 */
 	listEvents(calendarId, params) {
@@ -81,10 +86,7 @@ class CalendarAPI {
 
 		return this._getRequest(calendarId, `${gcalBaseUrl}${calendarId}/events`, params, this._JWT)
 			.then(resp => {
-				if (resp.statusCode !== 200) {
-					throw new Error(resp.statusCode + ':\n' + JSON.stringify(resp.body));
-				};
-
+				this._checkErrorResponse(200, resp.statusCode, resp.body);
 				let body = JSON.parse(resp.body);
 				return body.items;
 			}).catch(err => {
@@ -92,85 +94,135 @@ class CalendarAPI {
 			});
 	}
 
+
 	/**
-	 * Insert an event on the user's primary calendar. Returns promise of details of booking
+	 * Returns a promise that list all events on calendar during selected period
+	 * 
+	 * @param {string} calendarId 					- Calendar identifier
+	 * @param {datetime} params.timeMin (optional) 	- start datetime of event in 2016-04-29T14:00:00+08:00 RFC3339 format
+	 * @param {datetime} params.timeMax (optional) 	- end datetime of event in 2016-04-29T18:00:00+08:00 RFC3339 format
+	 * @param {string} params.q (optional) 			- Free text search terms to find events that match these terms in any field, except for extended properties. 
+	 * More Optional query parameters @ https://developers.google.com/google-apps/calendar/v3/reference/events/list
+	 */
+	getEvent(calendarId, eventId, params) {
+		this._checkCalendarId(calendarId, 'GetEvent');
+
+		return this._getRequest(calendarId, `${gcalBaseUrl}${calendarId}/events/${eventId}`, params, this._JWT)
+			.then(resp => {
+				this._checkErrorResponse(200, resp.statusCode, resp.body);
+				let body = JSON.parse(resp.body);
+				return body;
+			}).catch(err => {
+				throw new Error('GetEvent: ' + err);
+			});
+	}
+
+	/**
+	 * Insert an event on the calendar specified. Returns promise of details of event created within response body from google
 	 *
-	 * @param {string} summary 					- Event title to be specified in calendar event summary. Free-text
-	 * @param {nested object} start 			- start.dateTime defines start datetime of event in 2016-04-29T14:00:00+08:00 RFC3339 format
-	 * @param {nested object} end 				- end.dateTime defines end datetime of event in 2016-04-29T18:00:00+08:00 RFC3339 format
-	 * @param {string} location (optional) 		- Location description of event. Free-text
-	 * @param {string} description (optional) 	- Description of event.
-	 * @param {string} status (optional) 		- Event status - confirmed, tentative, cancelled; tentative for all queuing
-	 * @param {string} colorId (optional) 		- Color of the event
+	 * @param {string} calendarId 						- Calendar identifier
+	 * @param {string} params.summary 					- Event title to be specified in calendar event summary. Free-text
+	 * @param {nested object} params.start 				- start.dateTime defines start datetime of event in 2016-04-29T14:00:00+08:00 RFC3339 format
+	 * @param {nested object} params.end 				- end.dateTime defines end datetime of event in 2016-04-29T18:00:00+08:00 RFC3339 format
+	 * @param {string} params.location (optional) 		- Location description of event. Free-text
+	 * @param {string} params.description (optional) 	- Description of event.
+	 * @param {string} params.status (optional) 		- Event status - confirmed, tentative, cancelled; tentative for all queuing
+	 * @param {string} params.colorId (optional) 		- Color of the event
+	 * More Optional query parameters @ https://developers.google.com/google-apps/calendar/v3/reference/events/insert
 	 */
 	insertEvent(calendarId, params) {
 		this._checkCalendarId(calendarId, 'insertEvent');
 
 		return this._postRequest(calendarId, `${gcalBaseUrl}${calendarId}/events`, params, this._JWT)
 			.then(resp => {
-				if (resp.statusCode !== 200) {
-					throw new Error(resp.statusCode + ':\n' + JSON.stringify(resp.body));
-				};
-				return resp;
+				this._checkErrorResponse(200, resp.statusCode, resp.body);
+				return body;
 			})
 			.catch(err => {
 				throw new Error('InsertEvent: ' + err);
 			});
 	}
 
-	deleteEvent(calendarId, eventId) {
-		this._checkCalendarId(calendarId);
+	/**
+	 * Deletes an event on the calendar specified. Returns promise of details of booking
+	 *
+	 * @param {string} calendarId 					- Calendar identifier
+	 * @param {string} eventId 						- EventId specifying event to delete
+	 * @param {bool} params.sendNotifications (optional) 	- Whether to send notifications about the deletion of the event.
+	 */
+	deleteEvent(calendarId, eventId, params) {
+		this._checkCalendarId(calendarId, 'DeleteEvent');
 		if (eventId === undefined) {
-			throw new Error('Missing argument; need to pass in eventId');
+			throw new Error('deleteEvent: Missing argument; need to pass in eventId');
 		}
-
-		return requestWithJWT({
+		let options = {
 			method: 'DELETE',
-			url: 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events/' + eventId,
-			jwt: this._JWT
-		}).then(resp => {
-			if (resp.statusCode !== 204) {
-				throw new Error(resp.statusCode + ':\n' + resp.body);
-			}
-			let status = resp.statusCode;
+			url: `${gcalBaseUrl}${calendarId}/events/${eventId}`,
+			jwt: this._JWT,
+			qs: params
+		};
 
-			return { statusCode: status, message: 'Event delete success' };
-		})
+		return requestWithJWT(options)
+			.then(resp => {
+				this._checkErrorResponse(204, resp.statusCode, resp.body);
+				let status = resp.statusCode;
+				return { statusCode: status, message: 'Event delete success' };
+			})
 			.catch(err => {
-				throw err;
+				throw new Error('InsertEvent: ' + err);
 			});
 	}
 
 	/**
-	 * Checks when queried calendar is busy/ during selected period.
+	 * Returns instances of the specified recurring event.
+	 * 	 
+	 * @param {string} calendarId 					- Calendar identifier
+	 * @param {string} eventId 						- EventId specifying event to delete
+	*/
+	eventInstances(calendarId, eventId, params) {
+		this._checkCalendarId(calendarId, 'EventInstances');
+
+		return this._getRequest(calendarId, `${gcalBaseUrl}${calendarId}/events/${eventId}/instances`, params, this._JWT)
+			.then(resp => {
+				this._checkErrorResponse(200, resp.statusCode, resp.body);
+				let body = JSON.parse(resp.body);
+				return body.items;
+			})
+			.catch(err => {
+				throw new Error('EventInstances: ' + err);
+			});
+	}
+
+	/**
+	 * Checks when queried calendar is busy/ during selected time range.
 	 * Returns promise of list of start and end timings that are busy with time range.
 	 *
-	 * @param {string} startDateTime - start datetime of event in 2016-04-29T14:00:00+08:00 format
-	 * @param {string} endDateTime - end datetime of event in 2016-04-29T18:00:00+08:00 format
+	 * @param {string} calendarId		- Calendar identifier
+	 * @param {string} params.timeMin 	- start datetime of event in 2016-04-29T14:00:00+08:00 format
+	 * @param {string} params.timeMax 	- end datetime of event in 2016-04-29T18:00:00+08:00 format
+	 * @param {string} timeZone			- Timezone used in the response.
+	 * @param {string} items[].id		- The identifier of a calendar or a group to query
 	 */
-	checkBusyPeriod(calendarId, startDateTime, endDateTime) {
-		this._checkCalendarId(calendarId);
-		let event = {
-			"timeMin": startDateTime,
-			"timeMax": endDateTime,
-			"timeZone": this._TIMEZONE,
-			"items": [{ "id": calendarId }]
-		};
+	checkBusy(calendarId, params) {
+		this._checkCalendarId(calendarId, 'CheckBusy');
+		if (params.timeZone === undefined) {
+			params.timeZone = this._TIMEZONE;
+		}
 
-		let options = {
-			method: 'POST',
-			url: 'https://www.googleapis.com/calendar/v3/freeBusy',
-			json: true,
-			body: event,
-			jwt: this._JWT
-		};
-
-		return requestWithJWT(options).then(resp => {
-			return resp.body.calendars[calendarId].busy;
-		})
+		return this._postRequest(calendarId, 'https://www.googleapis.com/calendar/v3/freeBusy', params, this._JWT)
+			.then(resp => {
+				this._checkErrorResponse(200, resp.statusCode, resp.body);
+				return resp.body.calendars[calendarId].busy;
+			})
 			.catch(err => {
-				throw err;
+				throw new Error('CheckBusy: ' + err);
 			});
+
+		// return requestWithJWT(options).then(resp => {
+		// 	return resp.body.calendars[calendarId].busy;
+		// }).catch(err => {
+		// 	throw err;
+		// });
 	}
 }
 
