08747dacdb0e38e2c19ed902e904fae74aa35ffb
Shek Yu Hong <yuhong90@gmail.com>
2017-05-21 12:16:57 +0800

    Updated package version to 1.0.0.

--- config/Settings.js
+++ config/Settings.js
@@ -1,21 +1,18 @@
 //Sample CalendarAPI settings
 
-const KEYFILE = 'googleapi-keyfile.pem';			//path to pem key
 const SERVICE_ACCT_ID = '<your service account id>';
-
-const CALENDAR_URL = '<your calendar url>';
 const CALENDAR_ID = {
   'primary': '',
   'calendar-1': 'calendar1@group.calendar.google.com',
   'calendar-2': 'calendar2@group.calendar.google.com'
 };
 const TIMEZONE = 'UTC+08:00';
+const KEYFILE = 'googleapi-keyfile.pem';			//path to pem key
 
-module.exports.calendarId = CALENDAR_ID;
 module.exports.serviceAcctId = SERVICE_ACCT_ID;
-module.exports.keyfile = KEYFILE;			
-module.exports.calendarUrl = CALENDAR_URL;
+module.exports.calendarId = CALENDAR_ID;
 module.exports.timezone = TIMEZONE;
+module.exports.keyfile = KEYFILE;		
 
 //Example for using json keys
 // var fs = require('fs');
