3e38ebe1e4adb0abdfcc68e107bcc9fab2a41845
Shek Yu Hong <yuhong90@gmail.com>
2017-10-29 18:19:22 +0800

    Update sample code examples for using Event

--- example/eventExamples.js
+++ example/eventExamples.js
@@ -6,23 +6,24 @@ const calendarIdList = CONFIG.calendarId;
 eventExamples();
 
 function eventExamples() {
-	listAllEventsInCalendar(calendarIdList['test']);
-	// listSingleEventsWithinDateRange(calendarIdList['dev'], '2017-09-01T06:00:00+08:00', '2017-09-01T08:00:00+08:00', '');
+	listAllEventsInCalendar(calendarIdList['dev']);
+	// listSingleEventsWithinDateRange(calendarIdList['dev'], '2017-09-01T06:00:00+08:00', '2017-09-21T08:00:00+08:00', '');
 	// listRecurringEventsWithinDateRange(calendarIdList['test'], '2017-05-20T06:00:00+08:00', '2017-05-20T22:00:00+08:00', '');
-	// insertEvent(calendarIdList['test'], 'TestEventName', '2017-05-20T12:00:00+08:00', '2017-05-20T15:00:00+08:00', 'location', 'confirmed', 'some description here');
+	// insertEvent(calendarIdList['dev'], 'Test new event with notification', '2017-10-14T13:00:00+08:00', '2017-10-14T15:00:00+08:00', 'sg gds hive', 'confirmed', 'some description here');
+	// insertEventSample(calendarIdList['dev']);
 	// insertRecurringEvent(calendarIdList['test'], 'TestRecurringEvent', '2017-05-20T10:00:00+08:00', '2017-05-20T11:00:00+08:00', 'location', 'confirmed', 'description', ['RRULE:FREQ=WEEKLY;COUNT=3'])
 	// quickAddEvent(calendarIdList['test'], 'Breakfast 9am - 11am');
-	// getEvent(calendarIdList['primary'], 'algjb8m5jdjcgmptc3dqbcg3fc');
+	// getEvent(calendarIdList['dev'], 'mhf4u3evgdlb74n4n0lf1rqqi4');
 	// eventInstances(calendarIdList['drone'], '04fl5s82f98ccgp5dmba3132m0');
-	// updateEvent(calendarIdList['primary'], 'algjb8m5jdjcgmptc3dqbcg3fc', 'BreakfastEvent', '2017-05-20T08:30:00+08:00', '2017-05-20T11:00:00+08:00', '', 'confirmed', 'some descriptions here');
-	// moveEvent(calendarIdList['primary'], '04fl5s82f98ccgp5dmba3132m0', calendarIdList['test']);
-	// deleteEvent(calendarIdList['primary'], 'upnbq496e7k8l2fmctnph9jius');
+	// updateEvent(calendarIdList['dev'], 'iiubvdqfsh03u95p5tgovca5kc', 'Updated Event with reminder2', '2017-10-15T21:00:00+08:00', '2017-10-15T22:00:00+08:00', '', 'confirmed', 'some descriptions here');
+	// moveEvent(calendarIdList['test'], 'ri0io5ump79vlicbp5749m61jc', calendarIdList['dev']);
+	// deleteEvent(calendarIdList['dev'], '670ahicqj9q6hj5s2h92hb7rnc');
 }
 
 function listAllEventsInCalendar(calendarId) {
 	let eventsArray = [];
 	let params = {};
-	return cal.Events.list(calendarId, params)
+	return cal.Events.list(calendarId, params, {})
 		.then(json => {
 			for (let i = 0; i < json.length; i++) {
 				let event = {
@@ -39,7 +40,7 @@ function listAllEventsInCalendar(calendarId) {
 			console.log(eventsArray);
 			return eventsArray;
 		}).catch(err => {
-			console.log('Error: listAllEventsInCalendar -' + err);
+			console.log('Error: listAllEventsInCalendar', err.message);
 		});
 }
 
@@ -70,7 +71,7 @@ function listSingleEventsWithinDateRange(calendarId, startDateTime, endDateTime,
 			console.log(eventsArray);
 			return eventsArray;
 		}).catch(err => {
-			console.log('Error: listSingleEventsWithinDateRange -' + err);
+			console.log('Error: listSingleEventsWithinDateRange', err.message);
 		});
 }
 
@@ -100,21 +101,20 @@ function listRecurringEventsWithinDateRange(calendarId, startDateTime, endDateTi
 			console.log(eventsArray);
 			return eventsArray;
 		}).catch(err => {
-			console.log('Error: listRecurringEventsWithinDateRange -' + err);
+			console.log('Error: listRecurringEventsWithinDateRange', err.message);
 		});
 }
 
 function getEvent(calendarId, eventId) {
-	let params = {};
+	let params = { timeZone: 'Asia/Singapore' };
 
 	return cal.Events.get(calendarId, eventId, params)
 		.then(resp => {
-			console.log('GetEvent: ' + eventId);
 			console.log(resp);
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: getEvent-' + err);
+			console.log('Error: getEvent', JSON.parse(err.message));
 		});
 }
 
@@ -130,7 +130,7 @@ function quickAddEvent(calendarId, text) {
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: quickAddEvent-' + err);
+			console.log('Error: quickAddEvent', err.message);
 		});
 }
 
@@ -161,7 +161,38 @@ function insertEventWithAttendee(calendarId, eventSummary, startDateTime, endDat
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: insertEventWithAttendee-' + err);
+			console.log('Error: insertEventWithAttendee', err.message);
+		});
+}
+
+function insertEventSample(calendarId) {
+	let params = {
+		'summary': 'Sample Event with reminders',
+		'start': { 'dateTime': '2017-10-15T21:30:00+08:00' },
+		'end': { 'dateTime': '2017-10-15T22:00:00+08:00' },
+		'description': '',
+		'status': 'confirmed',
+		'colorId': 3,
+		"reminders": {
+			"useDefault": false,
+			"overrides": [
+				{ "method": "email", "minutes": 25 },
+				{ "method": "popup", "minutes": 20 }
+			]
+		},
+		'attendees': [{ 'email': 'yuhong90@gmail.com' }]
+	};
+	let optionalQueryParams = {
+		sendNotifications: true
+	};
+
+	return cal.Events.insert(calendarId, params, optionalQueryParams)
+		.then(resp => {
+			console.log('inserted event:');
+			console.log(resp);
+		})
+		.catch(err => {
+			console.log('Error: insertEvent', err.message);
 		});
 }
 
@@ -179,15 +210,18 @@ function insertEvent(calendarId, eventSummary, startDateTime, endDateTime, locat
 		'description': description,
 		'colorId': 1
 	};
+	let optionalQueryParams = {
+		sendNotifications: true
+	};
 
-	return cal.Events.insert(calendarId, event)
+	return cal.Events.insert(calendarId, event, optionalQueryParams)
 		.then(resp => {
+			console.log(resp);
 			let results = {
 				id: resp.id,
 				'summary': resp.summary,
 				'location': resp.location,
 				'status': resp.status,
-				'htmlLink': CALENDAR_URL,
 				'start': resp.start.dateTime,
 				'end': resp.end.dateTime,
 				'created': new Date(resp.created)
@@ -197,7 +231,7 @@ function insertEvent(calendarId, eventSummary, startDateTime, endDateTime, locat
 			return results;
 		})
 		.catch(err => {
-			console.log('Error: insertEvent-' + err);
+			console.log('Error: insertEvent', err.message);
 		});
 }
 
@@ -226,7 +260,7 @@ function insertRecurringEvent(calendarId, eventSummary, startDateTime, endDateTi
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: insertRecurringEvent-' + err);
+			console.log('Error: insertRecurringEvent-' + err.message);
 		});
 }
 
@@ -244,18 +278,24 @@ function updateEvent(calendarId, eventId, eventSummary, startDateTime, endDateTi
 		'summary': eventSummary,
 		'status': status,
 		'description': description,
-		'colorId': 1,
-		'recurrence': recurrenceRule
+		'recurrence': recurrenceRule,
+		'reminders': {
+			'useDefault': false,
+			'overrides':
+			[{ 'method': 'popup', 'minutes': '25' },
+			{ 'method': 'email', 'minutes': '20' }]
+		}
 	};
+	let optionalQueryParams = { sendNotifications: true };
 
-	cal.Events.update(calendarId, eventId, event)
+	cal.Events.update(calendarId, eventId, event, optionalQueryParams)
 		.then(resp => {
 			console.log('updated event:');
 			console.log(resp);
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: updatedEvent-' + err);
+			console.log('Error: updatedEvent', err.message);
 		});
 }
 
@@ -270,7 +310,7 @@ function deleteEvent(calendarId, eventId) {
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: deleteEvent-' + err);
+			console.log('Error: deleteEvent', JSON.parse(err.message));
 		});
 }
 
@@ -284,12 +324,12 @@ function eventInstances(calendarId, eventId) {
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: eventInstances-' + err);
+			console.log('Error: eventInstances' + err.message);
 		});
 }
 
 function moveEvent(calendarId, eventId, destination) {
-	let params = { 'destination': destination };
+	let params = { 'destination': destination, 'sendNotifications': true };
 
 	return cal.Events.move(calendarId, eventId, params)
 		.then(resp => {
@@ -298,6 +338,6 @@ function moveEvent(calendarId, eventId, destination) {
 			return resp;
 		})
 		.catch(err => {
-			console.log('Error: moveEvent-' + err);
+			console.log('Error: moveEvent', err.message);
 		});
 }
